7b01165e6bb97bed85f8028998e61b01d430051d
Jose Loarca <joseloarca97@icloud.com>
2026-06-09 13:42:27 -0400

    Updated loggers streaming config

--- logger/factory.py
+++ logger/factory.py
@@ -37,11 +37,7 @@ def get_session_logger() -> Logger:
         file_handler = FileHandler(logfile)
         file_handler.setFormatter(formatter)
 
-        console_handler = StreamHandler()
-        console_handler.setFormatter(formatter)
-
         logger.addHandler(file_handler)
-        logger.addHandler(console_handler)
 
     _logger_instance = logger
     return logger
